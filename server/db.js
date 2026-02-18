53431c55ab81ea2b1263d464cfd6835fae55fad4
shoponlinesainfo-cloud <shoponlinesainfo@gmail.com>
2026-02-18 10:50:13 +0200

    Create server/db.js file for MongoDB connection with Mongoose

---
+++ server/db.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/mydatabase'; // Change this to your MongoDB URI
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;
