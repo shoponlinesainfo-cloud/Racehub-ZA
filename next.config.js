33a540238db0657a4d3d8c1b46e03b43ae9c9e1e
shoponlinesainfo-cloud <shoponlinesainfo@gmail.com>
2026-02-18 10:50:25 +0200

    Create next.config.js for Next.js 14 configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+'use client';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add your custom settings here
+};
+
+module.exports = nextConfig;
