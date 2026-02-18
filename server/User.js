f6f6a8cb51097b7610b8580acb417e4cd69f2206
shoponlinesainfo-cloud <shoponlinesainfo@gmail.com>
2026-02-18 10:50:08 +0200

    Create User.js file with Mongoose User schema and bcrypt password hashing

---
+++ server/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
