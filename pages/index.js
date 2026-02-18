7e30f45f095918f3647bd5c44650cf32e341975f
shoponlinesainfo-cloud <shoponlinesainfo@gmail.com>
2026-02-18 10:49:35 +0200

    Create pages/index.js for RaceHub ZA home page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// Home page for RaceHub ZA
+
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to RaceHub ZA</h1>
+            <p>Your ultimate destination for racing news and resources.</p>
+        </div>
+    );
+};
+
+export default HomePage;
